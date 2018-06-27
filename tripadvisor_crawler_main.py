2b3a494954080010bff7285eb20483418c4f858b
Ye Wang <yewang0910@gmail.com>
2018-06-27 00:37:57 -0700

    update

--- tripadvisor_crawler_main.py
+++ tripadvisor_crawler_main.py
@@ -23,11 +23,11 @@ if __name__ == '__main__':
         # print '===get_hotel_tripadvisor_link'
         # get_hotel_tripadvisor_link(path)
         #
-        print '===get_hotel_tripadvisor_summary'
-        get_hotel_tripadvisor_summary(path)
+        # print '===get_hotel_tripadvisor_summary'
+        # get_hotel_tripadvisor_summary(path)
         #
-        # print '===get_hotel_tripadvisor_reviews'
-        # get_hotel_tripadvisor_reviews(path)
+        print '===get_hotel_tripadvisor_reviews'
+        get_hotel_tripadvisor_reviews(path)
 
         print '===update_hotel_reviewer_name'
         # crawl_hotel_reviewer_name(path, reviewer_path)
@@ -44,7 +44,7 @@ if __name__ == '__main__':
         stop = timeit.default_timer()
 
         print stop - start
-        break
+        # break
 
 # Tripadvisor_Hotel_Name
 # Reviews_num
